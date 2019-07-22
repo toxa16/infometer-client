e982547b32c330b760141e8ea4ebfd4416afeea7
toxa16 <toxa16@gmail.com>
2019-07-22 23:02:46 +0300

    homepage & tos routing

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer className="container d-flex flex-wrap justify-content-between pb-2">
-      <a className="text-info text-nowrap pr-3" href="#">Terms of Service</a>
+      <Link className="text-info text-nowrap pr-3" to="/terms-of-service">
+        Terms of Service
+      </Link>
       <span className="text-secondary text-nowrap">© 2019 Anton Bahurinsky</span>
     </footer>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="/">InfoMeter</a>
+        <Link className="navbar-brand" to="/">InfoMeter</Link>
         <a className="text-info" href="#"><i className="fab fa-twitter"></i></a>
       </div>
     </nav>
