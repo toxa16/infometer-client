8ef44dc828eed44fcec585d66dcb179dd09a9582
toxa16 <toxa16@gmail.com>
2019-07-22 22:58:52 +0300

    init terms of service

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Nav from './Nav';
 import Footer from './Footer';
 import Homepage from './Homepage';
+import TermsOfService from './TermsOfService';
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-      <article className="h-100 d-flex flex-column justify-content-between">
-        <Nav />
+        <article className="h-100 d-flex flex-column justify-content-between">
+          <Nav />
 
-        <main className="container">
-          <div className="row text-center">
-            <div className="col-12 col-md-10 col-lg-8 mx-auto">
+          <main className="container">
+            <div className="row text-center">
+              <div className="col-12 col-md-10 col-lg-8 mx-auto">
 
-            <Route exact path="/" component={ Homepage } />
+              <Route exact path="/" component={ Homepage } />
+              <Route path="/terms-of-service" component={ TermsOfService } />
 
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
 
-        <div className="clearfix"></div>
+          <div className="clearfix"></div>
 
-        <Footer />
-      </article>
+          <Footer />
+        </article>
       </Router>
     );
   }
---
+++ src/TermsOfService.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TermsOfService() {
+  return (
+    <div>
+      <h2 className="text-info mb-3">Terms of Service</h2>
+      <p className="text-justify text-muted">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Fusce risus lorem, scelerisque nec mollis sit amet, vestibulum id felis. 
+        Suspendisse eleifend leo vitae consectetur mollis. Cras quis pulvinar odio. 
+        Ut et ipsum non ligula mollis mollis. Pellentesque imperdiet ut odio quis pretium. 
+        Aenean tristique tellus dapibus auctor iaculis. Suspendisse non sapien tortor.
+      </p>
+    </div>
+  );
+}
