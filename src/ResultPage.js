0708ea24649c22c2842ec4d99b3332cb44fece74
toxa16 <toxa16@gmail.com>
2019-07-23 10:49:41 +0300

    init random percentage

--- src/ResultPage.js
+++ src/ResultPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export default class ResultPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      infoPercentage: null,
+    };
+  }
+
   componentDidMount() {
-    console.log(this.props.q);
+    const randomNumber = Math.round(Math.random() * 100);
+    this.setState({
+      infoPercentage: randomNumber,
+    });
   }
 
   handleBackClick(event) {
@@ -14,8 +24,10 @@ export default class ResultPage extends React.Component {
     return (
       <div>
         <p className="h3">Your info is</p>
-        <p className="font-weight-bold text-info display-1">19%</p>
-        <p className="text-muted pb-5">"Kate loves me"</p>
+        <p className="font-weight-bold text-info display-1">
+          { this.state.infoPercentage }%
+        </p>
+        <p className="text-muted pb-5">"{ this.props.q }"</p>
         <div>
           <a className="text-info" href="/"
               onClick={ this.handleBackClick.bind(this) }>Try another info</a>
